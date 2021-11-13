53ab386fe177efabc431027fd73365c934a01ca5
RomeelTeja7 <rom3607@gmail.com>
2021-11-12 21:00:37 -0600

    stilos

--- js/stilo_tabla.js
+++ js/stilo_tabla.js
@@ -1,7 +1,90 @@
+//colores de la tabla
 $(document).ready(function() {
-    $('a').click(function() {
-      $('#ciclo1').css({
+    $('#fondo1').click(function() {
+      $('table').css({
          "backgroundColor": "yellow"
       });
     });
   });
+  $(document).ready(function() {
+    $('#fondo2').click(function() {
+      $('table').css({
+         "backgroundColor": "pink"
+      });
+    });
+  });
+  $(document).ready(function() {
+    $('#fondo3').click(function() {
+      $('table').css({
+         "backgroundColor": "gray"
+      });
+    });
+  });
+
+  //estilo de texto de la tabla
+  //colores de la tabla
+$(document).ready(function() {
+  $('#texto1').click(function() {
+    $('table').css({
+       "font-family": "cursive"
+    });
+  });
+});
+$(document).ready(function() {
+  $('#texto2').click(function() {
+    $('table').css({
+      "font-family": "fantasy"
+    });
+  });
+});
+$(document).ready(function() {
+  $('#texto3').click(function() {
+    $('table').css({
+      "font-family": "monospace"
+    });
+  });
+});
+
+//colores de fondo
+$(document).ready(function() {
+  $('#back1').click(function() {
+    $('#divca1').css({
+      "backgroundColor": "red"
+    });
+  });
+});
+$(document).ready(function() {
+  $('#back2').click(function() {
+    $('#divca1').css({
+      "backgroundColor": "orange"
+    });
+  });
+});
+$(document).ready(function() {
+  $('#back3').click(function() {
+    $('#divca1').css({
+      "backgroundColor": "purple"
+    });
+  });
+});
+$(document).ready(function() {
+  $('#back1').click(function() {
+    $('body').css({
+      "backgroundColor": "red"
+    });
+  });
+});
+$(document).ready(function() {
+  $('#back2').click(function() {
+    $('body').css({
+      "backgroundColor": "orange"
+    });
+  });
+});
+$(document).ready(function() {
+  $('#back3').click(function() {
+    $('body').css({
+      "backgroundColor": "purple"
+    });
+  });
+});
